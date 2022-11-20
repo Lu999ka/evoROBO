3f9667c32cf387c8e3dfb329f222b580e15b1689
Lu999ka <118304082+Lu999ka@users.noreply.github.com>
2022-11-20 15:55:06 +0100

    Adding our first link to our environment and moving it

--- generate.py
+++ generate.py
@@ -1,1 +1,7 @@
+import pyrosim.pyrosim as pyrosim
 
+pyrosim.Start_SDF("box.sdf")
+
+pyrosim.Send_Cube("box.sdf", [0, 0, 0.5], [1, 1, 1])
+
+pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -2,7 +2,9 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
+p.loadSDF("box.sdf")
 for sec in range(1000):
     time.sleep(1/60)
+    p.stepSimulation()
 p.disconnect()
 
