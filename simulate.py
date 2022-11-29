e3a62a0e4c04b1d2351813ef56c3725f16ce0446
Lu999ka <118304082+Lu999ka@users.noreply.github.com>
2022-11-29 10:23:10 +0100

    Adding motors to our bot and trying to make it move

--- simulate.py
+++ simulate.py
@@ -3,11 +3,15 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import time
 import numpy
+import math
+
+pi = math.pi
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
+print(robotId)
 p.setGravity(0, 0, -9.81)
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
@@ -19,6 +23,12 @@ for sec in range(1000):
     p.stepSimulation()
     back_leg_sensor_values[sec] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     front_leg_sensor_values[sec] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b"Torso_FrontLeg", controlMode=p.POSITION_CONTROL,
+                                targetPosition=+pi/4, maxForce=500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b"Torso_BackLeg", controlMode=p.POSITION_CONTROL,
+                                targetPosition=-pi/4, maxForce=500)
+    
+
 numpy.save(file="data/backLegSensorValues.npy", arr=back_leg_sensor_values)
 numpy.save(file="data/frontLegSensorValues.npy", arr=front_leg_sensor_values)
 p.disconnect()
