bf0fc11f90b9461084e43d6e34b0544964724e14
Lu999ka <118304082+Lu999ka@users.noreply.github.com>
2022-12-06 10:54:21 +0100

    Adding sinusoidal movement to motors of legs, and adding varying parrameters to modify the difference between these motions

--- analize.py
+++ analize.py
@@ -1,11 +1,19 @@
 import numpy
 import matplotlib.pyplot
 
-back_leg_sensor_values = numpy.load("data/backLegSensorValues.npy")
-front_leg_sensor_values = numpy.load("data/frontLegSensorValues.npy")
-print(back_leg_sensor_values)
+# back_leg_sensor_values = numpy.load("data/sinValues.npy")
+# front_leg_sensor_values = numpy.load("data/cosValues.npy")
+# print(back_leg_sensor_values)
+#
+#
+# matplotlib.pyplot.plot(back_leg_sensor_values, label="Back Leg", linewidth=2)
+# matplotlib.pyplot.plot(front_leg_sensor_values, label="Front leg", linewidth=0.8)
+# matplotlib.pyplot.legend()
+# matplotlib.pyplot.show()
 
-matplotlib.pyplot.plot(back_leg_sensor_values, label="Back Leg", linewidth=2)
-matplotlib.pyplot.plot(front_leg_sensor_values, label="Front leg", linewidth=0.8)
-matplotlib.pyplot.legend()
+backLeg = numpy.load("data/backLeg.npy")
+frontLeg = numpy.load("data/frontLeg.npy")
+matplotlib.pyplot.plot(backLeg)
+matplotlib.pyplot.plot(frontLeg)
+
 matplotlib.pyplot.show()
--- simulate.py
+++ simulate.py
@@ -3,9 +3,18 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import time
 import numpy
-import math
+import matplotlib.pyplot as plt
+import random
 
-pi = math.pi
+pi = numpy.pi
+
+amplitude_fl = pi/4
+frequency_fl = 5
+phaseOffset_fl = 1
+amplitude_bl = pi/4
+frequency_bl = 5
+phaseOffset_bl = 0
+
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -18,18 +27,30 @@ pyrosim.Prepare_To_Simulate(robotId)
 back_leg_sensor_values = numpy.zeros(1000)
 front_leg_sensor_values = numpy.zeros(1000)
 
+angles = numpy.linspace(0, 2*pi, 1000)
+
+back_leg_angles = amplitude_bl * numpy.sin(frequency_bl*angles + phaseOffset_bl)
+front_leg_angles = amplitude_fl * numpy.sin(frequency_fl*angles + phaseOffset_fl)
+
+numpy.save(file="data/frontLeg.npy", arr=front_leg_angles)
+numpy.save(file="data/backLeg.npy", arr=back_leg_angles)
+
+# exit()
+
 for sec in range(1000):
-    time.sleep(1/60)
+    time.sleep(1/240)
     p.stepSimulation()
+
     back_leg_sensor_values[sec] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     front_leg_sensor_values[sec] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b"Torso_FrontLeg", controlMode=p.POSITION_CONTROL,
-                                targetPosition=+pi/4, maxForce=500)
+                                targetPosition=front_leg_angles[sec], maxForce=100)
     pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b"Torso_BackLeg", controlMode=p.POSITION_CONTROL,
-                                targetPosition=-pi/4, maxForce=500)
-    
+                                targetPosition=back_leg_angles[sec], maxForce=100)
+
 
 numpy.save(file="data/backLegSensorValues.npy", arr=back_leg_sensor_values)
 numpy.save(file="data/frontLegSensorValues.npy", arr=front_leg_sensor_values)
+
 p.disconnect()
 
